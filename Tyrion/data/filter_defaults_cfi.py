63262c8407f05145db9781b272767a77a2c177c6
Camilo Salazar <camilo.salazar@cern.ch>
2017-09-11 10:39:05 +0200

    Changes tu use  FWCore.ParameterSet.Config, but have problems to load from filter_cfg.py

--- Tyrion/data/filter_defaults_cfi.py
+++ Tyrion/data/filter_defaults_cfi.py
@@ -1,6 +1,8 @@
-module VLFFilter = Tyrion
-{
+import FWCore.ParameterSet.Config as cms
+
+#module VLFFilter = Tyrion
+#{
 #  InputTag TrackProducerTag         = rsWithMaterialTracks
 #  uint32 MinimalNumberOfTracks      = 0
 #  uint32 MaximalNumberOfTracks      = 999999
-}
+#}
